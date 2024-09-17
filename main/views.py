8a62168c3f46fbbcc8217020f92275d9507cc64c
na-62 <nafisa.arrasyida@gmail.com>
2024-09-18 04:12:54 +0700

    added assignment 3 implementations

--- main/views.py
+++ main/views.py
@@ -1,15 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from main.forms import ProductEntryForm
+from main.models import Product
+from django.http import HttpResponse
+from django.core import serializers
 
 def show_main(request):
+    product_entries = Product.objects.all()
+
     context = {
         'app_name' : 'Ink & Imagination',
         'npm' : '2306226391',
         'name': 'Nafisa Arrasyida',
         'class': 'PBP E',
-        'product_name':'A5 Sketchbook',
-        'product_price': 120000,
-        'product_description':'hot pressed, 60 pages',
-        'product_media':'watercolor, ink, graphite'
+        'product_entries': product_entries
     }
 
     return render(request, "main.html", context)
+
+def create_product_entry(request):
+    form = ProductEntryForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return redirect('main:show_main')
+
+    context = {'form': form}
+    return render(request, "create_product_entry.html", context)
+
+def show_xml(request):
+    data = Product.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = Product.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = Product.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json_by_id(request, id):
+    data = Product.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
