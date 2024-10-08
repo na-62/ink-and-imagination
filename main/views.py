55856b53d1a46d6e17b59c69ca37ad706a8b6697
na-62 <nafisa.arrasyida@gmail.com>
2024-10-08 20:12:29 +0700

    implementasi sebelum ajax

--- main/views.py
+++ main/views.py
@@ -9,17 +9,19 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from django.views.decorators.csrf import csrf_exempt
-from django.views.decorators.http import require_POST
+
 
 @login_required(login_url='/login')
 def show_main(request):
+    product_entries = Product.objects.filter(user=request.user)
+
     context = {
         'app_name' : 'Ink & Imagination',
         'npm' : '2306226391',
         'name': 'Nafisa Arrasyida',
         'class': 'PBP E',
         'user': request.user,
+        'product_entries': product_entries,
         'last_login': request.COOKIES['last_login'],
     }
 
@@ -59,24 +61,6 @@ def delete_product(request, id):
     # Kembali ke halaman awal
     return HttpResponseRedirect(reverse('main:show_main'))
 
-@csrf_exempt
-@require_POST
-def add_product_ajax(request):
-    product_name = request.POST.get("name")
-    price = request.POST.get("price")
-    description = request.POST.get("description")
-    media = request.POST.get("media")
-    user = request.user
-
-    new_product = Product(
-        name=product_name, price=price,
-        description=description, media=media,
-        user=user
-    )
-    new_product.save()
-
-    return HttpResponse(b"CREATED", status=201)
-
 def register(request):
     form = UserCreationForm()
 
@@ -114,11 +98,11 @@ def logout_user(request):
     return response
 
 def show_xml(request):
-    data = Product.objects.filter(user=request.user)
+    data = Product.objects.all()
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
 def show_json(request):
-    data = Product.objects.filter(user=request.user)
+    data = Product.objects.all()
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 def show_xml_by_id(request, id):
